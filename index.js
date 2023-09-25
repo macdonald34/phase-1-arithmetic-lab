306b8f4cc828776ba68f447a891d7c4f39ca8df3
macdonald34 <144123866+macdonald34@users.noreply.github.com>
2023-09-25 09:56:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() *10)+1;
+console.log(random);
+
+let num3 = 44,
+num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(5,20,13,15);
